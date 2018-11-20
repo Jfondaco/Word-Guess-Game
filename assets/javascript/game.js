b3593163c49c1a2675a2dd309978a3af2ae7bb78
Joey Fondaco <Jfondacoo@gmail.com>
2018-11-20 17:36:08 -0500

    adding local storage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,22 @@
-// Current issues:
-// -Lets someone guess the same letter twice
-// -if answer has 2 of the same letter it adds 2 guesses remaining back (should be 1)
-// -if answer has 2 of the same letter remaining guesses only decreases by 1 (should be 2)
-// -need to restart when word is finished or guesses remaining = 0 and increment wins
+function setState(state) {
+  localStorage["word_guess_game"] = JSON.stringify(state);
+}
 
+function getState() {
+  if (!"word_guess_game" in localStorage) {
+    setState({'score': 0});
+  }
+  return JSON.parse(localStorage["word_guess_game"]);
+}
+
+function incrementScore() {
+    state = getState();
+    state.score++;
+    setState(state);
+}
 
 // create variables here
-var score = 0;
+//var score = 0;
 var guessesLeft = 12;
 
 //grab p ids
@@ -30,7 +40,7 @@ var answerArray = [];
 for (var i=0; i < answer.length; i++){
     answerArray[i] = "_";
 }
-    
+
 //Function for guessing
 document.onkeyup = function(event) {
 
@@ -39,30 +49,17 @@ document.onkeyup = function(event) {
     // registers key press.
     var userGuess = event.key;
 
-    //reduce guessesLeft
-    guessesLeft--;
-
-    
-
-    //need a loop where it will check the guessesMade array to see if guess was already made
-        //if guess wasn't made yet push it to array
-        if (guessesMade.indexOf(userGuess) === -1)
-            {
-            guessesMade.push(userGuess);
-            }
-         
-        //(below doesn't work) if guess is already in the guessMade arrayy add a guess left back
-        if (guessesMade.indexOf(userGuess) === 1){
-            guessesLeft++;
-        }
-    
-    //loops checking if guess matches a letter from the answer array   
+    var guessedAlready = guessesMade.indexOf(userGuess) != -1;
+    if (!guessedAlready) {
+        guessesMade.push(userGuess);
+        guessesLeft--;
+        //loops checking if guess matches a letter from the answer array
         for (var j = 0; j < answer.length; j++) {
-            // && guessesMade.indexOf(answer[j]) === -1 TRIED THIS WITH && IN IF LOOP // 
+            // && guessesMade.indexOf(answer[j]) === -1 TRIED THIS WITH && IN IF LOOP //
             if (answer[j] === userGuess) {
-            answerArray[j] = userGuess;
-            guessesLeft++;
-            }    
+                answerArray[j] = userGuess;
+                guessesLeft++;
+            }
 
             if (answerArray.includes("_")){
                 moreToGo = true;
@@ -70,38 +67,45 @@ document.onkeyup = function(event) {
             else {
                 moreToGo = false;
             }
-            
-        }  
+
+        }
 
         if (moreToGo === false){
-            score++
-            // need some restart function here?
+            incrementScore();
+           
+            if(confirm("Congrats!")) {
+                window.location.reload();
+            }
         }
 
         if (guessesLeft === 0){
-            alert("YOU LOSE");
-            // also need restart function here
+            
+            if(confirm("You Lose!")) {
+                window.location.reload();
+            }
+            
         }
+
+        //display results and progress
+        guessesText.textContent = "Guesses remaining " + guessesLeft;
+        winsText.textContent = "Wins: " + getState().score;
+        lettersText.textContent = "Letters Guessed: " + guessesMade + " ";
+        progress.textContent = answerArray;
+
+        //need a statement for a new game if guessesLeft = 0 OR if answer is complete
+        // if ((remainingLetters === 0)
+        // wins ++ and a reload page function
+
+        // (guessesLeft === 0)){}
+        // you lose alert and a reload page function
+
+        console.log(moreToGo);
+        console.log(answerArray)
+        console.log(answer)
+
+    } 
     
-    
-    
-//display results and progress
-guessesText.textContent = "Guesses remaining " + guessesLeft; 
-winsText.textContent = "Wins: " + score;
-lettersText.textContent = "Letters Guessed: " + guessesMade + " ";
-progress.textContent = answerArray;    
-
-//need a statement for a new game if guessesLeft = 0 OR if answer is complete
-// if ((remainingLetters === 0) 
-// wins ++ and a reload page function
-
-// (guessesLeft === 0)){}
-// you lose alert and a reload page function
-
-console.log(moreToGo);
-console.log(answerArray)
-console.log(answer)
-
+    else {
+        alert("You already guessed " + userGuess);
+    }
 }
-
-
